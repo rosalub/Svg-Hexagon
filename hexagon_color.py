9a9e9d6d68518e93d822003ec60a8473c0d02482
rosalub <rosalie.dauchez.etu@univ-lille.fr>
2023-10-25 09:34:48 +0200

    feat: Added documentation

--- hexagon_color.py
+++ hexagon_color.py
@@ -2,6 +2,17 @@
 from PIL import Image
  
 def get_color(img_path, origins) : 
+    """
+    Take an image and a list of coordinates and return 
+    the color of the associated pixels  
+
+    Args :
+    - img_path : (str) path of the image file
+    - origins : (list) tuples associated to coordinates 
+
+    Returns :
+    - pixels : (list) list of the color of each pixels 
+    """
     # creating an image object
     im = Image.open(rf'{img_path}')
     px = im.load()

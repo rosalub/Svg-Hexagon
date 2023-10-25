7d16098ac7759c27c36197cce9f6665277e29484
FrancoisRoussel <144619154+FrancoisRoussel@users.noreply.github.com>
2023-10-25 09:45:15 +0200

    fix: hexatess function error

--- hexagon_shaping/tesselate.py
+++ hexagon_shaping/tesselate.py
@@ -32,7 +32,6 @@ def hexatess(l, img):
     """Tesselates a given picture by hexagons
 =======
      """Tesselates a given picture by hexagons
->>>>>>> 78aeba7a9073574127e5bb2523643fb186f159ba
     Args:
     - l: (int) length of the hexagon
     - img: (str) path of the image
